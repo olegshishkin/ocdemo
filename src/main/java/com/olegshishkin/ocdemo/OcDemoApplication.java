f02baf0ad48c32b945ad4d1382dd549636f02f61
Oleg Shishkin <olegshishkin@gmail.com>
2020-06-04 09:18:11 +0300

    Migrate app to WildFly

--- src/main/java/com/olegshishkin/ocdemo/OcDemoApplication.java
+++ src/main/java/com/olegshishkin/ocdemo/OcDemoApplication.java
@@ -3,6 +3,8 @@ package com.olegshishkin.ocdemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +12,7 @@ import java.util.List;
 
 @RestController
 @SpringBootApplication
-public class OcDemoApplication {
+public class OcDemoApplication extends SpringBootServletInitializer {
 
 	private final PersonService service;
 
@@ -31,6 +33,13 @@ public class OcDemoApplication {
 				.build();
 		return service.save(p);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
+	}
+
+	private static Class<OcDemoApplication> applicationClass = OcDemoApplication.class;
 
 	public static void main(String[] args) {
 		SpringApplication.run(OcDemoApplication.class, args);
