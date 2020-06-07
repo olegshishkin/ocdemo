b22374bb93ca4d4d4997a16b97abba7a48d0a673
Oleg Shishkin <olegshishkin@gmail.com>
2020-06-07 22:10:03 +0300

    add root rq mapping

--- src/main/java/com/olegshishkin/ocdemo/OcDemoApplication.java
+++ src/main/java/com/olegshishkin/ocdemo/OcDemoApplication.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
+@RequestMapping("/ocdemo")
 @RestController
 @SpringBootApplication
 public class OcDemoApplication {
