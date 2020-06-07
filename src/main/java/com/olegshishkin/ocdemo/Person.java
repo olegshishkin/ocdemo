0fb7eaeae69a71fdfcef67af65d26916d04a7df9
Oleg Shishkin <olegshishkin@gmail.com>
2020-06-08 00:27:42 +0300

    add identity for pk

--- src/main/java/com/olegshishkin/ocdemo/Person.java
+++ src/main/java/com/olegshishkin/ocdemo/Person.java
@@ -9,6 +9,8 @@ import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Setter
@@ -22,6 +24,8 @@ import javax.persistence.Id;
 public class Person {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false, updatable = false)
     private Long id;
 
     @Column(unique = true, nullable = false)
